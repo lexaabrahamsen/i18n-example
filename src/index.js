c09df810602a322655f44234cae745d53413f91f
Lexa <lexaraea@gmail.com>
2020-04-21 11:22:55 -0400

    Add FormattedMessage

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { IntlProvider } from 'react-intl';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <IntlProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </IntlProvider>,
   document.getElementById('root')
 );
 
